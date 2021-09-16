ec14e799518dea6954578029903f725c11247442
Galia Muslimova <galia.musllimova@gmail.com>
2021-09-16 09:12:10 +0500

    modified:   src/includes/footer/footer.pug 	modified:   src/includes/footer/footer.scss 	modified:   src/includes/footer/rights/rights.pug 	modified:   src/includes/footer/rights/rights.scss 	modified:   src/includes/header/header.scss 	modified:   src/index.js 	modified:   src/index.scss 	modified:   src/pages/main/main.scss

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-import '/index.scss';
+import '@/fonts/fonts.css';
+
+import '@/index.scss';
+
+import '@/includes/includes.scss';
+
+import '@/assets/assets.scss';
+
+import '@/pages/main/main.scss';
+
+
+
 
