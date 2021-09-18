9361e9f134889ad09b106bf9ffaaebe674d2cc04
Galia Muslimova <galia.musllimova@gmail.com>
2021-09-18 17:27:21 +0500

    modified:   src/assets/input/calendar/calendar.scss 	modified:   src/assets/input/input.js 	modified:   src/assets/input/input.scss 	new file:   src/assets/input/select/select-inner/count/count.pug 	new file:   src/assets/input/select/select-inner/count/count.scss 	new file:   src/assets/input/select/select-inner/select-inner.pug 	new file:   src/assets/input/select/select-inner/select-inner.scss 	new file:   src/assets/input/select/select.js 	modified:   src/assets/input/select/select.pug 	modified:   src/assets/input/select/select.scss 	modified:   src/includes/footer/links/links.scss 	modified:   src/index.scss 	modified:   src/pages/main/entry/entry.scss

--- src/assets/input/input.js
+++ src/assets/input/input.js
@@ -1,3 +1,5 @@
+import '@/assets/input/select/select.js'
+
 $(document).ready(function(event) {
   $('.input__btn').click(function() {
     let id = $(this).parent()[0].id;
---
+++ src/assets/input/select/select.js
@@ -0,0 +1,8 @@
+$(document).ready(function (event) {
+  $('.count__btn').click(function () {
+    if($(this).hasClass('plus')) {
+      let id = true;
+      console.log(id);
+    }    
+  })
+})
