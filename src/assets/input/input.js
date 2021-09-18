fbc895e95db6179784462b8303dbf6c71b5349f4
Galia Muslimova <galia.musllimova@gmail.com>
2021-09-18 11:24:53 +0500

    modified:   src/assets/assets.scss 	modified:   src/assets/button/button.scss 	deleted:    src/assets/calendar/calendar.js 	deleted:    src/assets/calendar/calendar.pug 	deleted:    src/assets/calendar/calendar.scss 	new file:   src/assets/input/arrow-btn.svg 	new file:   src/assets/input/calendar/calendar.pug 	new file:   src/assets/input/calendar/calendar.scss 	renamed:    src/assets/input/input__btn.svg -> src/assets/input/dropdown-btn.svg 	modified:   src/assets/input/input.js 	modified:   src/assets/input/input.pug 	modified:   src/assets/input/input.scss 	deleted:    src/assets/select/select.pug 	deleted:    src/assets/select/select.scss 	modified:   src/includes/entrySet/entrySet.pug 	modified:   src/includes/footer/links/links.pug 	modified:   src/includes/header/header.pug 	modified:   src/index.scss 	modified:   src/pages/main/entry/entry.pug 	modified:   src/pages/main/entry/entry.scss

--- src/assets/input/input.js
+++ src/assets/input/input.js
@@ -1,6 +1,7 @@
 $(document).ready(function(event) {
   $('.input__btn').click(function() {
     let id = $(this).parent()[0].id;
+    console.log(id);
     $('#' + id + '-input').toggleClass('active');
   })
 })
