7b6cec674b5729e1f129360e934c9d2ac8aa3229
Galia Muslimova <galia.musllimova@gmail.com>
2021-10-16 15:20:08 +0500

    modified:   src/components/drop/drop.scss 	new file:   src/components/form/form.pug 	modified:   src/components/form/form.scss 	modified:   src/components/header/header.js 	modified:   src/components/header/header.pug 	modified:   src/components/header/header.scss 	new file:   src/components/header/nav-btn/nav-btn.pug 	new file:   src/components/header/nav-btn/nav-btn.scss 	modified:   src/index.js 	new file:   src/pages/empty/empty.pug 	new file:   src/ui-kit/cards/cards.pug 	new file:   src/ui-kit/cards/cards.scss 	new file:   src/ui-kit/colors/colors.json 	new file:   src/ui-kit/colors/colors.pug 	new file:   src/ui-kit/colors/colors.scss 	new file:   src/ui-kit/forms/forms.pug 	new file:   src/ui-kit/forms/forms.scss 	new file:   src/ui-kit/insertions/insertions.pug

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
   $('.nav-btn').on('click', function() {
-    $('nav').toggleClass('active');
+    $('.menu-list').toggleClass('active-menu');
     $('.nav-btn').toggleClass('open-menu');
     $('body').toggleClass('fixed-page');
   });
 
   $('.btn_header').click(function() {
     $(location).attr('href', "./login.html");
+  })
+
+  $("nav i").click(function() {
+    $(this).closest('li').find('.drop').toggleClass('active')
   })
 });
--- src/index.js
+++ src/index.js
@@ -11,3 +11,6 @@ requireAll(require.context('@c', true, /\.scss$/));
 
 requireAll(require.context('./pages', true, /\.js$/));
 requireAll(require.context('./pages', true, /\.scss$/));
+
+requireAll(require.context('./ui-kit', true, /\.js$/));
+requireAll(require.context('./ui-kit', true, /\.scss$/));
