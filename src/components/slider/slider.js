8e69e796dbad0d7227d224b21c20bdd0674673f1
Galia Muslimova <galia.musllimova@gmail.com>
2021-10-18 13:16:31 +0500

    modified:   src/components/button/button.scss 	modified:   src/components/catalog-list/catalog-list.pug 	modified:   src/components/catalog-list/catalog-list.scss 	deleted:    src/components/catalog-turn/catalog-turn.pug 	modified:   src/components/filter/filter.pug 	renamed:    src/components/catalog-turn/catalog-turn.scss -> src/components/like/like.js 	modified:   src/components/like/like.pug 	modified:   src/components/like/like.scss 	new file:   src/components/pagination/pagination.pug 	new file:   src/components/pagination/pagination.scss 	modified:   src/components/radio/radio.scss 	new file:   src/components/rate/rate.pug 	new file:   src/components/rate/rate.scss 	modified:   src/components/slider/slider.js 	modified:   src/components/slider/slider.pug 	modified:   src/components/slider/slider.scss 	modified:   src/components/toggle/toggle.scss 	modified:   src/pages/catalog/catalog.pug 	modified:   src/theme/fonts.scss 	modified:   src/ui-kit/forms/forms.pug 	modified:   src/ui-kit/forms/forms.scss

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -1,8 +1,32 @@
 import * as noUiSlider from 'nouislider';
 import 'nouislider/dist/nouislider.css';
+import "@c/slider/slider.scss"
 
 $(window).on('load', function () {
-  let slider = document.getElementById('slider-round');
+
+  function createSlider(id) {
+    let slider = document.getElementById(id + "-slider-round");
+
+    noUiSlider.create(slider, {
+      start: [5000, 10000],
+      behaviour: 'drag',
+      connect: true,
+      range: {
+        'min': 0,
+        'max': 15000
+      }
+    });
+
+    slider.noUiSlider.on("slide", function (values) {
+      $("#" + id + "-slider-result").text(parseInt(values[0]) + '₽ - ' + parseInt(values[1]) + '₽');
+    })
+
+    let result = slider.noUiSlider.get();
+    $("#" + id + "-slider-result").text(parseInt(result[0]) + '₽ - ' + parseInt(result[1]) + '₽');
+  }
+
+  createSlider("forms")
+  /*let slider = document.getElementById('slider-round');
 
   noUiSlider.create(slider, {
     start: [5000, 10000],
@@ -19,5 +43,5 @@ $(window).on('load', function () {
   })
 
   let result = slider.noUiSlider.get();
-  $('#slider-result').text(parseInt(result[0]) + '₽ - ' + parseInt(result[1]) + '₽');
+  $('#slider-result').text(parseInt(result[0]) + '₽ - ' + parseInt(result[1]) + '₽');*/
 })
