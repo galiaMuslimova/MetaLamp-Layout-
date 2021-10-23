ce44eca1cb541b459df3419972712ffcea1dddb5
Galia Muslimova <galia.musllimova@gmail.com>
2021-10-23 09:48:06 +0500

    modified:   src/components/carousel/carousel.js 	modified:   src/components/carousel/carousel.pug 	modified:   src/components/carousel/carousel.scss

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,8 +1,38 @@
 $(document).ready(function() {
-  let n = 888;
-  function requireAll(requireContext) { 
-    return requireContext.keys().filter(el => el.includes(n)).map(requireContext)
-  };
-  let images = requireAll(require.context("@c/catalog-list/img/", true, /\.(jpg|png|jpeg)$/));
-  console.log(images);
+  $(".carousel__mover").on("click", function() {     
+    let images = $(this).siblings(".carousel__image");
+    let indicators = $(this).siblings(".carousel__indicators");
+
+    function slideToRight (itemList) {
+      let activeItem = itemList.find(".shown");
+      if (activeItem.next()[0] != undefined) {
+        activeItem.next().addClass("shown");
+      } else {
+        let firstItem = itemList.children().first();
+        $(firstItem).addClass("shown");
+      }
+      activeItem.removeClass("shown")
+    }
+
+    function slideToLeft(itemList) {
+      let activeItem = itemList.find(".shown");
+      if (activeItem.prev()[0] != undefined) {
+        activeItem.prev().addClass("shown");
+      } else {
+        let lastItem = itemList.children().last();
+        $(lastItem).addClass("shown");
+      }
+      activeItem.removeClass("shown");
+    }
+
+    if($(this).hasClass("carousel__mover_right")) {      
+      slideToRight(images);
+      slideToRight(indicators)
+    }
+
+    if ($(this).hasClass("carousel__mover_left")) {
+      slideToLeft(images);
+      slideToLeft(indicators);
+    }
+  })
 })
