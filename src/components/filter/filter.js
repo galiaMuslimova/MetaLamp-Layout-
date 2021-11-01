83be70fe8f96ea39dcaf7c0649484fe64c2a783e
Galia Muslimova <galia.musllimova@gmail.com>
2021-11-01 17:48:52 +0500

    modified:   src/components/checkbox/checkbox.scss 	modified:   src/components/filter/filter.js 	modified:   src/components/filter/filter.pug 	modified:   src/components/filter/filter.scss 	modified:   src/components/form/form.scss 	modified:   src/components/select/select.scss

--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -4,4 +4,7 @@ $(".filter").ready(function() {
   let datepicker_filter = makeCalendar(".datepicker_filter");
   let filterDate = ['2019-08-19', '2019-08-23'];
   datepicker_filter.selectDate(filterDate);
+  $(".filter__expandable").find(".input__open").on("click", function(){
+    $(this).toggleClass("active")
+  })
 })  
