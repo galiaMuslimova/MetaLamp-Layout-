00dfb03271cbb16ca8b8c0931f3554b511d880f2
Galia Muslimova <galia.musllimova@gmail.com>
2022-01-14 13:14:43 +0500

    remove all mixes 4/4, add classes, change names of tags and change structure of folders by the way #9, #10, #13, #15

--- src/ui-kit/forms/forms.js
+++ src/ui-kit/forms/forms.js
@@ -5,5 +5,12 @@ $(() => {
   if ($formsSlider.length > 0) {
     createSlider($formsSlider);
   }  
-  $('.js-expanded').find('.js-drop').addClass('active');
+
+  $('.js-form__select_expanded').find('.js-drop').addClass('drop_active');
+  $('.js-form__expandable_expanded').find('.js-drop').addClass('drop_active');
+
+  const baseElement = $('.js-pagination__anchor_forms');
+  const paginationItems = $('.js-catalog-list .js-catalog-item')
+
+  createPagination(baseElement, paginationItems)
 });
