b64cd15e709817afd2f88420a8c7006a5b365706
Galia Muslimova <galia.musllimova@gmail.com>
2022-02-23 10:09:44 +0500

    make dynamic rate #4

---
+++ src/components/rate/Rate.js
@@ -0,0 +1,29 @@
+class Rate {
+  constructor(element) {
+    this.element = $(element);
+    this.$stars = this.element.find('.js-rate__star');
+    this.bindEventListeners();
+  }
+
+  bindEventListeners() {
+    this.$stars.on('click', this.handleRateStarClick);
+  }
+
+  handleRateStarClick() {
+    let prevStar = $(this).prev();
+    let nextStar = $(this).next();
+    while (prevStar.length > 0) {
+      prevStar.addClass('rate__star_colored');
+      prevStar = prevStar.prev();
+    }
+    while (nextStar.length > 0) {
+      nextStar.removeClass('rate__star_colored');
+      nextStar = nextStar.next();
+    }
+    $(this).addClass('rate__star_colored');
+  }
+}
+
+$('.js-rate').each(function () {
+  const rate = new Rate(this);
+});
